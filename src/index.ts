23e664556b1cae565ddb5330d1bedf9204cc9a76
Marcos Corsi <marcosvcorsi@gmail.com>
2021-06-24 21:52:42 -0300

    feat: add xlsx create test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import { createWriteStream, fstat, WriteStream } from 'fs';
+import { createWriteStream, WriteStream } from 'fs';
 import { resolve } from 'path';
+import * as XLSX from 'xlsx';
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -23,22 +24,37 @@ const writeResults = (results: Array<any>, ws: WriteStream) => {
   results.map((item: any) => writeLine(Object.values(item), ws));
 }
 
+const appendResultsXlsx = (xlsxFileName: string, results: Array<any>) => {
+  const workbook = XLSX.readFile(xlsxFileName);
+}
+
 (async () => {
   const fields = ['name', 'url'];
 
   const writer = createWriteStream(resolve(__dirname, '..', 'out', 'out.csv'));
+  
+  const workbook = XLSX.utils.book_new();
+  const workSheetName = 'data';
+  workbook.SheetNames.push(workSheetName);
 
   writeLine(fields, writer);
-
+  
   const offset = 0;
   const limit = 100;
-
+  
   const initialData = await getDataFromAPI(offset, limit);
-
+  
   const { count, results } = initialData;
-
+  
   writeResults(results, writer);
+  
+  const ws = XLSX.utils.json_to_sheet(results, {header: fields });
+  workbook.Sheets[workSheetName] = ws;
 
+
+  const xlsxFilename = resolve(__dirname, '..', 'out', 'out.xlsx');
+  XLSX.writeFile(workbook, xlsxFilename);
+  
   let offsetCount = limit;
 
   while(offsetCount < count) {
